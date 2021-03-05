22661bc9970b34dee8e3a6cc4b22490eed515050
Alex Pereira Vieira <alex.p.vieira@gmail.com>
2021-03-05 00:54:20 +0000

    Finalizando o exercício

--- src/main/java/br/com/impacta/lab/Pessoa.java
+++ src/main/java/br/com/impacta/lab/Pessoa.java
@@ -2,6 +2,8 @@ package br.com.impacta.lab;
 
 public interface Pessoa {
 
+  public void setDocumento(String documento);
+  public void setTipo(String tipo);
 	public String falarDocumento();
 	public String getTipo();
 	
